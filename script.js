650cc73a5af49cf21db86680c982a2df256b23a9
tevahdesign <tevahdesign@gmail.com>
2025-09-21 20:26:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2885050972904135"
+     crossorigin="anonymous"></script>
+<!-- New -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2885050972904135"
+     data-ad-slot="6755008128"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
